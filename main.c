86997b51129fd15ec389b174e2d823161a172f81
nalonso_facturante <nahuel.alonso@facturante.com>
2019-05-18 15:24:36 -0300

    add dump function

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ struct stack {
 int newStack(struct stack *);
 int pushStack(struct stack *, int data);
 int popStack(struct stack *, struct element *);
+int dumpStack(struct stack *);
 int isEmptyStack(struct stack);
 
 int main()
@@ -36,18 +37,18 @@ int main()
 
 
     /*
-     *
+     * Inserción de elementos en la pila
      *
      */
     int count = 1;
     int toInsert;
-    while(count < 100 && toInsert > 0){
+    while(count == 1 || toInsert > 0){
         printf( "Ingrese un Número para insertar en la pila (si no desea insertar más coloque -1)\n");
         scanf("%d", &toInsert );
-        pushStack(&tempStack, toInsert);
-        count++;
+        if(toInsert >0)
+            pushStack(&tempStack, toInsert);
     }
-
+    dumpStack(&tempStack);
 
     printf("Hello world!\n");
     return 0;
@@ -82,6 +83,16 @@ int popStack(struct stack *p, struct element *elementOut){
 
 int isEmptyStack(struct stack p){
 	if (p.size == 0) { return 1; }
+	return 0;
+}
+
+int dumpStack(struct stack *p){
+	if (p->size == 0) { return 1; }
+	struct element *toShow = p->top;
+	while(toShow != NULL){
+        printf( "Elemento con dato = %d\n", toShow->data);
+        toShow = toShow->next;
+	}
 	return 0;
 }
 
